04b4cc66a35ef39835a6f1fc19ebeed5dc11d222
dwitaaeni <witaaeni06@gmail.com>
2021-10-25 12:51:45 +1100

    menambahkan ss

---
+++ Tugas (Ariqoh) .py
@@ -0,0 +1,20 @@
+Python 3.10.0 (tags/v3.10.0:b494f59, Oct  4 2021, 19:00:18) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+========================= RESTART: D:/Tugas(Ariqoh) .py ========================
+Tugas Python
+
+Masukan Nama Anda :Ariqoh Zulaika Zuhrah
+Masukan Panggilan :Ariqoh
+Masukan NIM :312110138
+Masukan Tanggal Lahir :01 Oktober 2003
+Masukan Tempat Lahir :Jakarta
+Masukan Telepon :089529827380
+Masukan Alamat :Perumahan Telaga Murni Blok D 19 No.4 Kecamatan Cikarang Barat kabupate bekasi
+Masukan Umur Anda :18
+
+Assalamua'laikum
+
+Let me introduce my self. my name is Ariqoh Zulaika Zuhrah but you can call me Ariqoh My NIM is 312110138 I was born in Jakarta and I am 18 years old. I am very gald if you want to invite my house in Perumahan Telaga Murni Blok D 19 No.4 Kecamatan Cikarang Barat kabupate bekasi . So, don't forget to call me before with the number 089529827380
+
+THANK YOU
